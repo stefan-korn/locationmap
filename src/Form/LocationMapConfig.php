f23f5190bc3b144b126a17dd8fec6e7cc9488be5
Stefan Korn <drupal@stefan-korn.de>
2016-11-09 18:09:31 +0100

    Add Configuration Form First Version

---
+++ src/Form/LocationMapConfig.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace Drupal\locationmap\Form;
+
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * Implements the LocationMapConfig form controller.
+ *
+ * This example demonstrates a simple form with a singe text input element. We
+ * extend FormBase which is the simplest form base class used in Drupal.
+ *
+ * @see \Drupal\Core\Form\FormBase
+ */
+class LocationMapConfig extends FormBase {
+
+  /**
+   * Build the LocationMapConfigForm form.
+   *
+   * A build form method constructs an array that defines how markup and
+   * other form elements are included in an HTML form.
+   *
+   * @param array $form
+   *   Default form array structure.
+   * @param FormStateInterface $form_state
+   *   Object containing current form state.
+   *
+   * @return array
+   *   The render array defining the elements of the form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
+
+   $config = \Drupal::config('locationmap.settings');    
+    $form['mapsapikey'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('API Key'),
+      '#description' => $this->t('Google Maps API Key'),
+      '#default_value' => $config->get('locationmap.mapsapikey'),
+      '#required' => TRUE,
+    ];
+
+    // Group submit handlers in an actions element with a key of "actions" so
+    // that it gets styled correctly, and so that other modules may add actions
+    // to the form. This is not required, but is convention.
+    $form['actions'] = [
+      '#type' => 'actions',
+    ];
+
+    // Add a submit button that handles the submission of the form.
+    $form['actions']['submit'] = [
+      '#type' => 'submit',
+      '#value' => $this->t('Submit'),
+    ];
+
+    return $form;
+  }
+
+  /**
+   * Getter method for Form ID.
+   *
+   * The form ID is used in implementations of hook_form_alter() to allow other
+   * modules to alter the render array built by this form controller.  it must
+   * be unique site wide. It normally starts with the providing module's name.
+   *
+   * @return string
+   *   The unique ID of the form defined by this class.
+   */
+  public function getFormId() {
+    return 'locationmap_config_form';
+  }
+
+  /**
+   * Implements form validation.
+   *
+   * The validateForm method is the default method called to validate input on
+   * a form.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    //$title = $form_state->getValue('title');
+    /*if (strlen($title) < 5) {
+      // Set an error for the form element with a key of "title".
+      $form_state->setErrorByName('title', $this->t('The title must be at least 5 characters long.'));
+    }*/
+  }
+
+  /**
+   * Implements a form submit handler.
+   *
+   * The submitForm method is the default method called for any submit elements.
+   *
+   * @param array $form
+   *   The render array of the currently built form.
+   * @param FormStateInterface $form_state
+   *   Object describing the current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    /*
+     * This would normally be replaced by code that actually does something
+     * with the title.
+     */
+    
+    $mapsapikey = $form_state->getValue('mapsapikey');    
+    $config = \Drupal::service('config.factory')->getEditable('locationmap.settings');
+    $config->set('locationmap.mapsapikey', $mapsapikey);
+    $config->save();
+    drupal_set_message(t('Location Map succesfully configured'));
+  }
+
+}
